c2ffccf51030967f10e04c9d48da9b5c2dcb2a69
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2024-08-23 09:48:54 +0530

    updated convigs

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
+type EdgeLocals = import('@astrojs/vercel').EdgeLocals
+
+declare namespace App {
+  interface Locals extends EdgeLocals {
+    // ...
+  }
+}
